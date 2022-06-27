36ddee09afbcc815f06db52142161185f33c11ad
Elliot Jones <38585421+emjones95@users.noreply.github.com>
2022-06-27 20:14:04 +1200

    Add files via upload

--- data_download.R
+++ data_download.R
@@ -11,32 +11,32 @@ library(readxl) #useful for reading in a spreadsheet
 
 # 2022 World Happiness Report 
 url1 <- "https://happiness-report.s3.amazonaws.com/2022/Appendix_2_Data_for_Figure_2.1.xls"
-download.file(url1, destfile = "whr22_rank.xls")
+download.file(url1, destfile = "raw_data/whr22_rank.xls")
 
 # 2021 World Happiness Report 
 url2 <- "https://happiness-report.s3.amazonaws.com/2021/DataForFigure2.1WHR2021C2.xls"
-download.file(url2, destfile = "whr21_rank.xls")
+download.file(url2, destfile = "raw_data/whr21_rank.xls")
 
 url3 <- "https://happiness-report.s3.amazonaws.com/2021/DataPanelWHR2021C2.xls"
-download.file(url3, destfile = "whr21_panel_data.xls")
+download.file(url3, destfile = "raw_data/whr21_panel_data.xls")
 
 # 2020 World Happiness Report
 url4 <- "https://happiness-report.s3.amazonaws.com/2020/WHR20_DataForFigure2.1.xls"
-download.file(url4, destfile="whr20_rank.xls")
+download.file(url4, destfile="raw_data/whr20_rank.xls")
 
 url5 <- "https://happiness-report.s3.amazonaws.com/2020/WHR20_DataForTable2.1.xls"
-download.file(url5, destfile="whr20_panel_data.xls")
+download.file(url5, destfile="raw_data/whr20_panel_data.xls")
 
 # read in the data
-whr22_rank <- read_excel("whr22_rank.xls")
-whr21_rank<- read_excel("whr21_rank.xls")
-whr21_panel_data <- read_excel("whr21_panel_data.xls")
-whr20_rank <- read_excel("whr20_rank.xls")
-whr21_panel_data <- read_excel("whr20_panel_data.xls")
+whr22_rank <- read_excel("raw_data/whr22_rank.xls")
+whr21_rank<- read_excel("raw_data/whr21_rank.xls")
+whr21_panel_data <- read_excel("raw_data/whr21_panel_data.xls")
+whr20_rank <- read_excel("raw_data/whr20_rank.xls")
+whr21_panel_data <- read_excel("raw_data/whr20_panel_data.xls")
 
 # ONS wellbeing data for the United Kingdom
 url4 <- "https://download.ons.gov.uk/downloads/datasets/wellbeing-quarterly/editions/time-series/versions/4.xlsx"
-download.file(url4, destfile="ons_data.xlsx")
+download.file(url4, destfile="raw_data/ons_data.xlsx")
 
 # read in the data
-ons_data <- read_excel("ons_data.xlsx", skip = 2)
+ons_data <- read_excel("raw_data/ons_data.xlsx", skip = 2)
